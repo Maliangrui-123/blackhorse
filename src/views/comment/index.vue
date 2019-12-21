97090e6b9c7bab07b10e5c594c024ec2a4bd7bb1
Maliang <965932942@qq.com>
2019-12-21 20:37:15 +0800

    评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,28 +1,59 @@
 <template>
   <el-card>
-      <!-- 面包屑给了卡片的具名插槽 -->
+    <!-- 面包屑给了卡片的具名插槽 -->
     <bread-crumb slot="header">
-    <!-- 插槽内容 -->
-        <template slot="title">评论列表</template>
+      <!-- 插槽内容 -->
+      <template slot="title">评论列表</template>
     </bread-crumb>
     <!-- body内容 -->
-    <el-table>
+    <el-table :data="list">
       <!-- 表格列组件 label 设置表头 -->
-      <el-table-column width="600" label="标题"></el-table-column>
-      <el-table-column label="评论状态"></el-table-column>
-      <el-table-column label="总评论数"></el-table-column>
-      <el-table-column label="粉丝评论数"></el-table-column>
-      <el-table-column label="操作"></el-table-column>
+      <el-table-column prop="title" width="600" label="标题"></el-table-column>
+      <!-- 格式化属性来显示布尔值 -->
+      <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作">
+        <!-- 放组件  要使用作用域插槽-->
+        <template slot-scope="obj">
+          <el-button size="small" type="text">修改</el-button>
+          <el-button size="small" type="text">{{ obj.row.comment_status ? '关闭评论' : '打开评论' }}</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    //   请求评论列表数据
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results // 获取评论列表数据
+      })
+    },
+    // 定义一个布尔值转化方法
+    formatterBool (row, column, cellValue, index) {
+      // row是当前行数据，column是当前列属性，cellValue是当前单元格值，index是当前下标
+      //   formatter必须要有返回值
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    //   调用上面设置的请求数据方法
+    this.getComment()
+  }
 }
 </script>
 
 <style>
-
 </style>
