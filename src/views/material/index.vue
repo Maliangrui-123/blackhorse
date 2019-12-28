dd00dcfd37fd946a22b715d0bf124ca1f6d8c067
Maliang <965932942@qq.com>
2019-12-28 17:28:28 +0800

    素材列表数据加载完成

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,7 +5,7 @@
       <template slot="title">素材管理</template>
     </bread-crumb>
     <!-- 放置素材 -->
-    <el-tabs v-model="activeName">
+    <el-tabs v-model="activeName" @tab-click="changeTab">
       <el-tab-pane label="全部素材" name="all">
         <!-- 全部素材内容 需要定制一个样式 -->
         <div class="img-list">
@@ -19,7 +19,15 @@
           </el-card>
         </div>
       </el-tab-pane>
-      <el-tab-pane label="收藏素材" name="collect"></el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect">
+          <!-- 收藏素材内容 -->
+          <div class="img-list">
+          <!-- v-for渲染页面 -->
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+      </el-tab-pane>
     </el-tabs>
   </el-card>
 </template>
@@ -33,11 +41,15 @@ export default {
     }
   },
   methods: {
+    changeTab () {
+      this.getAllMaterial()
+    },
     //   获取所有素材
     getAllMaterial () {
+      // 当前的this.activeName === "collect"就查询收藏，
       this.$axios({
         url: '/user/images',
-        params: { collect: false }
+        params: { collect: this.activeName === 'collect' }
       }).then(result => {
         this.list = result.data.results
       })
@@ -69,7 +81,7 @@ export default {
         position: absolute;
         bottom: 0;
         background-color: #f4f5f6;
-        margin-left: -20px;
+        left: 0
     }
   }
 }
